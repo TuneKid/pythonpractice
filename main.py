e13bdfd439c307279a708c3b69b9c70747281411
TuneKid <joshua.mat.george@gmail.com>
2020-02-22 03:18:31 +0000

    I was very wise and masterfull, and did absolutely nothing.

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-
+import random
 print("hello grades")
+grades = ["A+", "A", "A-", "B"]
+people = ["Abey", "Sindhu", "Joshua", "Hunter"]
+for x in people:
+  grade = random.choice(grades)
+  if x == "Abey":
+    grade = "F- in all subjects"
+  if x == "Joshua":
+    grade = "A+++ in English"
+  print(x + " got an " + grade )
+  
